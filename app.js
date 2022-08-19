ea1ad2771c7bcedbf00c8c58e596955765ed86ce
vlad <vladmelnyk04@gmail.com>
2022-08-19 12:39:06 +0300

    add operators

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //1 - Змінні
 const firstName = 'Vlad'
 //const lastName = 'Melnyk'
-const age = 18
+//const age = 18
 const isProgrammer = true
 
 const _private = 'private'
@@ -14,5 +14,29 @@ const  withNum5 = '5'
 console.log('Імя людини: '+ firstName + ', а вік людини: ' + age)
 console.log(age)*/
 
+/*
 const lastName = prompt('Введіть прізвище')
-alert(firstName + ' ' + lastName)
+alert(firstName + ' ' + lastName)*/
+
+//3 - Оператори
+
+let currentYear = 2022
+const birthYear = 2004
+
+//const age = currentYear - birthYear
+
+const a = 10
+const b = 5
+
+let c = 32
+//c = c + a
+/*
+c +=a
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(++currentYear)
+console.log(--currentYear)
+console.log(c)*/
