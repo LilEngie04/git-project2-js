04f79974ff304731919c2b3fe24e753f1c1aac90
vlad <vladmelnyk04@gmail.com>
2022-08-22 11:12:54 +0300

    add strings

--- app.js
+++ app.js
@@ -1,71 +1,48 @@
-// 1 - Number
-
+/*const name = 'Влад'
+const age = 18
+function getAge() {
+    return age
+}*/
 /*
-const num = 42
-const float = 42.42
-const pow = 10e3
-console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER)
-console.log('Math.pow 53', Math.pow(2, 53) - 1)
-console.log('MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER)
-console.log('MAX_VALUE', Number.MAX_VALUE)
-console.log('MIN_VALUE', Number.MIN_VALUE)
-console.log('MAX_VALUE', Number.MAX_VALUE)
-console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY)
-console.log('NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY)
-console.log('2/0', 2/0)
-console.log(Number.NaN)
-console.log(typeof NaN)
-const weird = 2/undefined
-console.log(Number.isNaN(weird))
-console.log(Number.isNaN(42))
-console.log(Number.isFinite(Infinity))
-console.log(Number.isFinite(42))
-
-const stringInt = '40'
-const stringFloat = '40.42'
-console.log(Number.parseInt(stringInt) + 2)
-console.log(parseInt(stringInt) + 2)
-console.log(Number(stringInt) + 2)
-console.log(+stringInt + 2)
-console.log(parseFloat(stringFloat) + 2)
-console.log(+stringFloat + 2)
-
-console.log(0.4 + 0.2)
-console.log(+(0.4+0.2).toFixed(1))
-console.log(parseFloat((0.4+0.2).toFixed(1)))*/
-
-// 2 - BigInt
-
+//const output = 'Привіт, мене звати ' + name + ' і мій вік ' + age + ' років.'
+const output = `Привіт, мене звати ${name} і мій вік ${age < 20 ? 'A' : 'B'} років`
+*/
 /*
-console.log(90071992547409919999999n - 9007199254740991999999n)
-console.log(-90071992547409919999999n)
-//console.log(90071992547409919999999.23231n) // error
+const output = `
+    <div>This is div</div>
+    <p>this is p</p>
+`
+console.log(output)*/
 
-//console.log(10n-4) // error
-console.log(parseInt(10n) - 4)
-console.log(10n - BigInt(4))
-console.log(5n / 2n) // 2n*/
+/*const name = 'Влад'
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.charAt(2))
+console.log(name.indexOf('л'))
+console.log(name.startsWith('вл'))
+console.log(name.startsWith('Вл'))
+console.log(name.toLowerCase().startsWith('вл'))
+console.log(name.endsWith('ад'))
+console.log(name.repeat(3))
+const string = '      password    '
+console.log(string.trim())
+console.log(string.trimLeft())
+console.log(string.trimRight())*/
 
-// 3 - Math
-
-/*console.log(Math.E)
-console.log(Math.PI)
-
-console.log(Math.sqrt(25))
-console.log(Math.pow(5, 3))
-console.log(Math.abs(-42))
-console.log(Math.max(42, 12, 23, 11, 422))
-console.log(Math.min(42, 12, 23, 11, 422))
-console.log(Math.floor(4.9))
-console.log(Math.ceil(4.9))
-console.log(Math.round(4.4))
-console.log(Math.trunc(4.9))
-console.log(Math.random())*/
-
-// 4 - Example
-
-function getRandomBetween(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+function logPerson(s, name, age) {
+    if(age<0) {
+        age = 'Чудо не родилось'
+    }
+    return `${s[0]}${name}${s[1]}${age}${s[2]}`
 }
-console.log(getRandomBetween(10, 42))
+const personName = 'Влад'
+const personName2 = 'Макс'
+const personAge = 18
+const personAge2 = -10
 
+const output = logPerson`Імя: ${personName}, Вік: ${personAge}!`
+const output2 = logPerson`Імя: ${personName2}, Вік: ${personAge2}!`
+
+console.log(output)
+console.log(output2)
