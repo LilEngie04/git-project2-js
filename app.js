25b642220663189b62fad22f4553432f6ae55479
vlad <vladmelnyk04@gmail.com>
2022-08-23 11:00:01 +0300

    add async func

--- app.js
+++ app.js
@@ -1,90 +1,57 @@
-/*const person = {
-    name: 'Vlad',
-    age: 18,
-    isProgrammer: true,
-    languages: ['ua', 'ru', 'en'],
-    //'complex_key': 'Complex Value',
-    //['key_' + (1+3)]: 'Computed Key',
-    greet() {
-        console.log('greet from person')
-    },
-    info() {
-        console.info('Інформаця про людину на імя: ', this.name)
-    }
-}*/
+// Event Loop
+
+/*const timeout = setTimeout(() => {
+    console.log('After timeout')
+}, 2500)
+
+clearTimeout(timeout)
+
+const interval = setInterval(() => {
+    console.log('After timeout')
+}, 1000)
+
+clearInterval(interval)*/
 
 /*
-console.log(person.name)
-const ageKey = 'age'
-console.log(person[ageKey])
-console.log(person['complex_key'])
-person.greet()
+const delay = (callback, wait = 1000) => {
+    setTimeout(callback, wait)
+}
 
-person.age++
-person.languages.push('de')
-//person['key_4']=undefined
-delete person['key_4']
+delay(() => {
+    console.log('After 2 seconds')
+}, 2000)*/
 
-console.log(person)
-console.log(person['key_4'])
-
-const name = person.name
-const age = person.age
-const languages = person.languages
-
-const {name, age: personAge = 10, languages} =person*/
-//console.log(person)
+const delay = (wait = 1000) => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, wait)
+    })
+    return promise
+}
 
 /*
-for (let key in person) {
-    if (person.hasOwnProperty(key)) {
-        console.log('key:', key)
-        console.log('values: ', person[key])
-    }
-}*/
+delay(2500)
+    .then(() => {
+        console.log('After 2 seconds')
+    })
+    .catch(err => console.error(err))
+    .finally(() => console.log('Finally'))*/
 
-/*const keys = Object.keys(person)
-keys.forEach((key) => {
-    console.log('key: ', keys)
-    console.log('values: ', person[key])
-})*/
-//Context
-//person.info()
+const getData = () => new Promise(resolve => resolve([
+    1, 1, 2, 3, 5, 8, 13
+]))
+//getData().then(data => console.log(data))
 
-/*
-const logger = {
-    keys() {
-        console.log('Object Keys: ', Object.keys(this))
-    },
-
-    keysAndValues() {
-        //"key": value
-        Object.keys(this).forEach(key => {
-            console.log(`"${key}": ${this[key]}`)
-        })
-        //const self = this
-        Object.keys(this).forEach(function (key) {
-            console.log(`"${key}": ${this[key]}`)
-        }.bind(this))
-    },
-
-    withParams(top = false, between = false, bottom = false) {
-        if(top) {
-            console.log('------- Start -------')
-        }
-        Object.keys(this).forEach((key, index, array) => {
-            console.log(`"${key}": ${this[key]}`)
-            if(between && index !== array.length - 1) {
-                console.log('----------------')
-            }
-        })
-        if(bottom) {
-            console.log('------- End -------')
-        }
+async function asyncExample() {
+    try {
+        await delay(3000)
+        const data = await getData()
+        console.log('Data', data)
+    } catch (e) {
+        console.log(e)
+    } finally {
+        console.log('Finally')
     }
 }
-//const bound = logger.keys().bind(person)
-//bound()
-//logger.keysAndValues().call(person)
-logger.withParams.call(person, true, true, true)
-logger.withParams.apply(person, [true, true, true])*/
+asyncExample()
